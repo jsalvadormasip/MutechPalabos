17f04478a9d75c5843ecb895775104a7613ced8a
Orestis <orestis.malaspinas@pm.me>
2021-05-06 11:16:40 +0000

    Resolve "Add clang-format in CI". Clang-format now used to format all Palabos source code. Also an error is popped in CI when clang-format produces warnings for badly formatted code.

--- src/latticeBoltzmann/nearestNeighborLattices2D.hh
+++ src/latticeBoltzmann/nearestNeighborLattices2D.hh
@@ -5,7 +5,7 @@
  * own the IP rights for most of the code base. Since October 2019, the
  * Palabos project is maintained by the University of Geneva and accepts
  * source code contributions from the community.
- * 
+ *
  * Contact:
  * Jonas Latt
  * Computer Science Department
@@ -14,7 +14,7 @@
  * 1227 Carouge, Switzerland
  * jonas.latt@unige.ch
  *
- * The most recent release of Palabos can be downloaded at 
+ * The most recent release of Palabos can be downloaded at
  * <https://palabos.unige.ch/>
  *
  * The library Palabos is free software: you can redistribute it and/or
@@ -29,7 +29,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 /** \file
  * Descriptor for nearest-neighbor 2D lattices -- generic code.
@@ -43,63 +43,55 @@ namespace plb {
 
 namespace descriptors {
 
-    // D2Q9 ////////////////////////////////////////////////////////////
+// D2Q9 ////////////////////////////////////////////////////////////
 
-    template<typename T>
-    const T D2Q9Constants<T>::invD = (T)1 / (T) d;
+template <typename T>
+const T D2Q9Constants<T>::invD = (T)1 / (T)d;
 
-    template<typename T>
-    const int D2Q9Constants<T>::vicinity = 1;
+template <typename T>
+const int D2Q9Constants<T>::vicinity = 1;
 
-    template<typename T>
-    const int D2Q9Constants<T>::c
-        [D2Q9Constants<T>::q][D2Q9Constants<T>::d] =
-        {
-            { 0, 0},
-            {-1, 1}, {-1, 0}, {-1,-1}, { 0,-1},
-            { 1,-1}, { 1, 0}, { 1, 1}, { 0, 1}
-        };
+template <typename T>
+const int D2Q9Constants<T>::c[D2Q9Constants<T>::q][D2Q9Constants<T>::d] = {
+    {0, 0}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}};
 
-    template<typename T>
-    const int D2Q9Constants<T>::cNormSqr[D2Q9Constants<T>::q] =
-        { 0, 2, 1, 2, 1, 2, 1, 2, 1 };
+template <typename T>
+const int D2Q9Constants<T>::cNormSqr[D2Q9Constants<T>::q] = {0, 2, 1, 2, 1, 2, 1, 2, 1};
 
-    template<typename T>
-    const T D2Q9Constants<T>::t[D2Q9Constants<T>::q] =
-        {
-            (T)4/(T)9, (T)1/(T)36, (T)1/(T)9, (T)1/(T)36, (T)1/(T)9,
-                       (T)1/(T)36, (T)1/(T)9, (T)1/(T)36, (T)1/(T)9
-        };
+template <typename T>
+const T D2Q9Constants<T>::t[D2Q9Constants<T>::q] = {(T)4 / (T)9,  (T)1 / (T)36, (T)1 / (T)9,
+                                                    (T)1 / (T)36, (T)1 / (T)9,  (T)1 / (T)36,
+                                                    (T)1 / (T)9,  (T)1 / (T)36, (T)1 / (T)9};
 
-    template<typename T>
-    const T D2Q9Constants<T>::cs2 = (T)1 / (T)3;
+template <typename T>
+const T D2Q9Constants<T>::cs2 = (T)1 / (T)3;
 
-    template<typename T>
-    const T D2Q9Constants<T>::invCs2 = (T)3;
+template <typename T>
+const T D2Q9Constants<T>::invCs2 = (T)3;
 
-    template<typename T>
-    const char D2Q9Descriptor<T>::name[] = "D2Q9";
+template <typename T>
+const char D2Q9Descriptor<T>::name[] = "D2Q9";
 
-    template<typename T>
-    const char ForcedD2Q9Descriptor<T>::name[] = "ForcedD2Q9";
+template <typename T>
+const char ForcedD2Q9Descriptor<T>::name[] = "ForcedD2Q9";
 
-    template<typename T>
-    const char RhoBarJD2Q9Descriptor<T>::name[] = "RhoBarJD2Q9";
-    
-    template<typename T>
-    const char RhoBarVelocityPiNeqOmegaD2Q9Descriptor<T>::name[] = "RhoBarVelocityPiNeqOmegaD2Q9";
-    
-    template<typename T>
-    const char VelocityD2Q9Descriptor<T>::name[] = "VelocityD2Q9";
+template <typename T>
+const char RhoBarJD2Q9Descriptor<T>::name[] = "RhoBarJD2Q9";
 
-    template<typename T>
-    const char Tau1_D2Q9Descriptor<T>::name[] = "Tau1_D2Q9";
+template <typename T>
+const char RhoBarVelocityPiNeqOmegaD2Q9Descriptor<T>::name[] = "RhoBarVelocityPiNeqOmegaD2Q9";
 
-    template<typename T>
-    const char AbsorbingWaveD2Q9Descriptor<T>::name[] = "AbsorbingWave_D2Q9";
+template <typename T>
+const char VelocityD2Q9Descriptor<T>::name[] = "VelocityD2Q9";
 
-    template<typename T>
-    const char ExtSymTensorD2Q9Descriptor<T>::name[] = "ExtSymTensor_D2Q9";
+template <typename T>
+const char Tau1_D2Q9Descriptor<T>::name[] = "Tau1_D2Q9";
+
+template <typename T>
+const char AbsorbingWaveD2Q9Descriptor<T>::name[] = "AbsorbingWave_D2Q9";
+
+template <typename T>
+const char ExtSymTensorD2Q9Descriptor<T>::name[] = "ExtSymTensor_D2Q9";
 
 }  // namespace descriptors
 
