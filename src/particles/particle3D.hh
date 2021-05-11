38dde33d6d8b27846752f7e158b5425a2e70c83e
remy.petkantchin@unige.ch <remy.petkantchin@unige.ch>
2021-05-11 06:38:12 +0200

    Changed particles coupling with solidFraction field

--- src/particles/particle3D.hh
+++ src/particles/particle3D.hh
@@ -708,6 +708,7 @@ void VerletParticle3D<T,Descriptor>::fluidToParticle(BlockLattice3D<T,Descriptor
     Array<T,3> tmpVel;
     Array<T,3> fluidVelocity;
     fluidVelocity.resetToZero();
+    Dot3D intPos( (plint)position1[0], (plint)position1[1], (plint)position1[2] );
     for (plint iCell=0; iCell<8; ++iCell) {
         if (contained(pos[iCell].x,pos[iCell].y,pos[iCell].z, fluid.getBoundingBox())) {
             fluid.get(pos[iCell].x,pos[iCell].y,pos[iCell].z).computeVelocity(tmpVel);
@@ -715,8 +716,8 @@ void VerletParticle3D<T,Descriptor>::fluidToParticle(BlockLattice3D<T,Descriptor
         else {
             tmpVel.resetToZero();
         }
-        if (contained(pos, sF.getBoundingBox()))
-			fluidVelocity += weights[iCell]*tmpVel*scaling*(1.-sF.get((plint)position[0],(plint)position[1],(plint)position[2]));
+        if (contained(intPos, sF.getBoundingBox()))
+			fluidVelocity += weights[iCell]*tmpVel*scaling*(1.-sF.get(intPos.x, intPos.y, intPos.z));
 		else
 		    fluidVelocity += weights[iCell]*tmpVel*scaling;
     }
